85648db13669b38aa95d02910a31efc962c62fc0
Stephen OBrien <stephen.obrien4@emc.com>
2017-11-26 14:51:09 +0000

    Refactor, factor out copy functions into a class

---
+++ backupservice.py
@@ -0,0 +1,83 @@
+import hashlib
+import os
+import shutil
+import time
+
+
+class BackupService:
+    """
+    Backup files
+    """
+    def __init__(self, log):
+        self.__log = log
+        self.__buffersize = 64000
+
+    def copy_files(self, files, backup_root):
+        """
+        Copy the list of files into the given backup root directory
+        :param files: the list of files
+        :param backup_root: the root directory to backup into
+        """
+        for file in files:
+            if file.rstrip():
+                self.__copy_file(file, backup_root)
+
+    def __copy_file(self, file, backup_root):
+        """
+        Copy the given file to the configured backup location. This creates
+        the full directory structure of the file being copied, underneath the
+        backup_root directory. It will also create a directory named after the file
+        itself and when backing up will copy the file into this directory renaming
+        the backed up file to the current timestamp. Finally it will create a
+        file called "latest" every time we run a backup so we can trivially
+        compare the last backed up file with the file on the next backup run.
+        :param file: the file to backup.
+        """
+
+        def copy_timestamped(f, backup_dir):
+            """
+            Copy the given file to the directory, named just as a timestamp. Also
+            create a latest file, denoting the latest copy of this file.
+            :param f: the file to copy
+            :param backup_dir: the directory to copy into
+            """
+            timestamp = time.strftime("%d-%h-%y_%H-%M-%S")
+            shutil.copy(f, backup_dir + "/" + timestamp)
+            shutil.copy(f, backup_dir + "/latest")
+            self.__log.info("Backed up {} into {}".format(file, backup_dir + "/" + timestamp))
+
+        if os.path.isfile(file):
+            if not os.path.isabs(file):
+                self.__log.warning("{} is a relative path, and is not supported!\n"
+                                   "Please remove from file list. Ignoring and continuing.".format(file))
+                return
+
+            backup_dir = backup_root + os.path.abspath(file)
+            prev_backup = backup_dir + "/latest"
+            if os.path.isfile(prev_backup):
+
+                prev_hash = self.__hash_file(prev_backup)
+                new_hash = self.__hash_file(file)
+                if prev_hash != new_hash:
+                    copy_timestamped(file, backup_dir)
+                else:
+                    self.__log.info("{} is unchanged, not backing up ...".format(file))
+            else:
+                os.makedirs(backup_dir, exist_ok=True)
+                copy_timestamped(file, backup_dir)
+        else:
+            self.__log.warning("{} does not exist! Please remove from file list. Ignoring and continuing.".format(file))
+
+    def __hash_file(self, file_path):
+        """
+        Generate a SHA1 hash of a file
+        :param file_path: path to the file
+        :return: the SHA1 hash as a hexadecimal string
+        """
+        sha1 = hashlib.sha1()
+        with open(file_path, 'rb') as file:
+            data = file.read(self.__buffersize)
+            while data != b'':
+                sha1.update(data)
+                data = file.read(self.__buffersize)
+        return sha1.hexdigest()
--- timemachine.py
+++ timemachine.py
@@ -1,19 +1,11 @@
 import argparse
-import hashlib
 import logging
 import os
-import shutil
 import sys
 import threading
-import time
 
+from backupservice import BackupService
 from watchstore import WatchStore
-
-"""
-Defaults for the backup and config file locations.
-"""
-STORE = "backup"
-CONFIG_FILE = "config.dat"
 
 # Global log configuration
 log = logging.getLogger()
@@ -25,15 +17,15 @@ channel.setFormatter(formatter)
 log.addHandler(channel)
 
 
-def standalone(watch, backup):
+def standalone(watch, backup_service, backup_location):
     """
     Runs a backup of the files in the config every 60 seconds
     :param watch: the watch store object
-    :param backup: the location to backup to
+    :param backup_location: the location to backup to
     """
     log.info("Starting backup ...")
-    threading.Timer(60, lambda: standalone(watch, backup)).start()
-    copy_files(watch.list_files(), backup)
+    threading.Timer(60, lambda: standalone(watch, backup_location)).start()
+    backup_service.copy_files(watch.list_files(), backup_location)
     log.info("Backup complete ...")
 
 
@@ -44,130 +36,63 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description='File watcher.')
     group = parser.add_mutually_exclusive_group(required=False)
-    group.add_argument('--add', nargs=1, help='add file to the list of files being watched')
-    group.add_argument('--remove', nargs=1, help='remove file from the list of files being watched')
+    group.add_argument('--add', help='add file to the list of files being watched')
+    group.add_argument('--remove', help='remove file from the list of files being watched')
     group.add_argument('--list', action='store_true', help='list files being watched')
     group.add_argument('--standalone', action='store_true', help='launch the app, backing up every minute')
-    parser.add_argument('--config-location', nargs=1, help='location of the config file')
-    parser.add_argument('--backup-location', nargs=1, help='location of the backup directory')
+    parser.add_argument('--config-location', default="config.dat", help='location of the config file')
+    parser.add_argument('--backup-location', default="backup", help='location of the backup directory')
     return parser.parse_args()
 
 
-def hash_file(file_path):
+def handle_args(config_location, backup_location, args):
     """
-    Generate a SHA1 hash of a file
-    :param file_path: path to the file
-    :return: the SHA1 hash as a hexadecimal string
+    Handle the arguments passed
+    :param config_location: the config file location
+    :param backup_location: the config file location
+    :param args: the arguments
     """
-    buffersize = 64000
-    sha1 = hashlib.sha1()
-    with open(file_path, 'rb') as file:
-        data = file.read(buffersize)
-        while data != b'':
-            sha1.update(data)
-            data = file.read(buffersize)
-    return sha1.hexdigest()
-
-
-def copy_file(file, backup_root):
-    """
-    Copy the given file to the configured backup location. This creates
-    the full directory structure of the file being copied, underneath the
-    backup_root directory. It will also create a directory named after the file
-    itself and when backing up will copy the file into this directory renaming
-    the backed up file to the current timestamp. Finally it will create a
-    file called "latest" every time we run a backup so we can trivially
-    compare the last backed up file with the file on the next backup run.
-    :param file: the file to backup.
-    """
-
-    def copy_timestamped(f, backup_dir):
-        """
-        Copy the given file to the directory, named just as a timestamp. Also
-        create a latest file, denoting the latest copy of this file.
-        :param f: the file to copy
-        :param backup_dir: the directory to copy into
-        """
-        timestamp = time.strftime("%d-%h-%y_%H-%M-%S")
-        shutil.copy(f, backup_dir + "/" + timestamp)
-        shutil.copy(f, backup_dir + "/latest")
-        log.info("Backed up {} into {}".format(file, backup_dir + "/" + timestamp))
-
-    if os.path.isfile(file):
-        if not os.path.isabs(file):
-            log.warning("{} is a relative path, and is not supported!\n"
-                        "Please remove from file list. Ignoring and continuing.".format(file))
-            return
-
-        backup_dir = backup_root + os.path.abspath(file)
-        prev_backup = backup_dir + "/latest"
-        if os.path.isfile(prev_backup):
-
-            prev_hash = hash_file(prev_backup)
-            new_hash = hash_file(file)
-            if prev_hash != new_hash:
-                copy_timestamped(file, backup_dir)
-            else:
-                log.info("{} is unchanged, not backing up ...".format(file))
+    watch_store = WatchStore(config_location, log)
+    backup_service = BackupService(log)
+    if args.add is not None:
+        file = args.add
+        if os.path.isfile(file):
+            if not os.path.isabs(file):
+                raise ValueError("Relative paths are not supported!")
+            watch_store.add_file(file)
         else:
-            os.makedirs(backup_dir, exist_ok=True)
-            copy_timestamped(file, backup_dir)
+            raise FileExistsError("File {} does not exist!".format(args.add))
+    elif args.remove is not None:
+        watch_store.remove_file(args.remove)
+    elif args.list:
+        log.info("The following files are being monitored: ")
+        for file in watch_store.list_files():
+            log.info(file)
+    elif args.standalone:
+        log.info("Scheduling a backup run every minute ...")
+        standalone(watch_store, backup_service, backup_location)
     else:
-        log.warning("{} does not exist! Please remove from file list. Ignoring and continuing.".format(file))
-
-
-def copy_files(files, backup_root):
-    """
-    Copy the list of files into the given backup root directory
-    :param files: the list of files
-    :param backup_root: the root directory to backup into
-    """
-    for file in files:
-        if file.rstrip():
-            copy_file(file, backup_root)
+        files = watch_store.list_files()
+        if len(files) > 0:
+            backup_service.copy_files(files, backup_location)
+        else:
+            log.warning("Config file {} does not contain any files, nothing to do.".format(config_location))
 
 
 if __name__ == '__main__':
     args = parse_args()
 
     # Setup the config location
-    config = CONFIG_FILE
-    if args.config_location is not None:
-        config = args.config_location[0]
-        log.info("Config path set to ".format(os.path.abspath(config)))
+    config = args.config_location
 
     if not os.path.isfile(config):
         log.info("Config file does not exist, creating ...")
         open(config, 'w+')
 
     # Setup the backup location
-    backup = STORE
-    if args.backup_location is not None:
-        backup = args.backup_location[0]
-        log.info("Backup root path set to {}".format(os.path.abspath(backup)))
+    backup = args.backup_location
 
-    watch = WatchStore(config, log)
+    log.info("Backup root path set to {}".format(os.path.abspath(backup)))
+    log.info("Config path set to {}".format(os.path.abspath(config)))
 
-    if args.add is not None:
-        file = args.add[0]
-        if os.path.isfile(file):
-            if not os.path.isabs(file):
-                raise ValueError("Relative paths are not supported!")
-            watch.add_file(file)
-        else:
-            raise FileExistsError("File {} does not exist!".format(args.add[0]))
-    elif args.remove is not None:
-        watch.remove_file(args.remove[0])
-    elif args.list:
-        log.info("The following files are being monitored: ")
-        for file in watch.list_files():
-            log.info(file)
-    elif args.standalone:
-        log.info("Scheduling a backup run every minute ...")
-        standalone(watch, backup)
-    else:
-        files = watch.list_files()
-        if len(files) > 0:
-            copy_files(files, backup)
-        else:
-            log.warning("Config file {} does not contain any files, nothing to do.".format(config))
+    handle_args(config, backup, args)
